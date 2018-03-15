67b2126bcacf22743d4b8f18b372157daa2f219f
dunnec <conordunne4@gmail.com>
2018-03-15 18:37:12 +0000

    Movie CRUD, USER ID Association

---
+++ app/controllers/movies_controller.rb
@@ -0,0 +1,52 @@
+class MoviesController < ApplicationController
+	before_action :find_movie, only: [:show,:edit, :update, :destroy]
+
+	def index
+		@movies = Movie.all.order("created_at DESC")
+	end
+
+	def show		
+	end
+
+	def new
+		@movie = current_user.movies.build
+	end
+
+	def create
+		@movie = current_user.movies.build(movie_params)
+
+		if@movie.save
+			redirect_to root_path
+		else
+			render 'new'
+		end
+	end
+
+	def edit
+
+	end
+
+	def update
+		if @movie.update(movie_params)
+			redirect_to movie_path(@movie)
+		else
+			render 'edit'
+		end
+	end
+
+	def destroy
+		@movie.destroy
+		redirect_to root_path
+	end
+
+	private
+
+		def movie_params
+			params.require(:movie).permit(:title, :genre, :year, :description)
+		end
+
+		def find_movie
+			@movie = Movie.find(params[:id])
+		end
+
+end
---
+++ db/migrate/20180315163733_create_movies.rb
@@ -0,0 +1,12 @@
+class CreateMovies < ActiveRecord::Migration[5.1]
+  def change
+    create_table :movies do |t|
+      t.string :title
+      t.string :genre
+      t.integer :year
+      t.text :description
+
+      t.timestamps
+    end
+  end
+end
